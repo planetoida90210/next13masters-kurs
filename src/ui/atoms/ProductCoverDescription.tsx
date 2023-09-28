0aa14f6699262eafdb943417b3fe249c460fbe9f
Michał Bryk <galeriakoloru90210@gmail.com>
2023-09-28 14:19:26 +0200

    fix typo from product title to product name

--- src/ui/atoms/ProductCoverDescription.tsx
+++ src/ui/atoms/ProductCoverDescription.tsx
@@ -6,12 +6,12 @@ type ProductCoverDescriptionProps = {
 };
 
 export const ProductCoverDescription = ({
-	product: { title, category, price },
+	product: { name, category, price },
 }: ProductCoverDescriptionProps) => {
 	return (
 		<div className="flex justify-between p-2">
 			<div>
-				<h3 className="truncate font-medium">{title}</h3>
+				<h3 className="truncate font-medium">{name}</h3>
 				<p className="text-slate-700">{category}</p>
 			</div>
 			<p>{formatMoney(price)}</p>
