57cdf4af7adc4c3aed963d0a1543bc75a629a2b5
Michał Bryk <galeriakoloru90210@gmail.com>
2023-10-02 18:26:57 +0200

    change typo in ul data-testid

--- src/ui/organism/ProductList.tsx
+++ src/ui/organism/ProductList.tsx
@@ -9,7 +9,7 @@ export const ProductList = ({
 }) => {
 	return (
 		<ul
-			data-testid="product-list"
+			data-testid="products-list"
 			className="grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-8 lg:grid-cols-3 xl:grid-cols-4"
 		>
 			{products.map((product) => {
