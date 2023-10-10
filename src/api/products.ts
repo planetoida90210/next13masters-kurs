88e2a035b04c8beebf9e59e0b55271b515c892c0
Michał Bryk <galeriakoloru90210@gmail.com>
2023-10-10 23:41:54 +0200

    executegraphql

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,4 +1,5 @@
 import { type ProductItemType } from "@/ui/types";
+import { type TypedDocumentString } from "@/gql/graphql";
 
 type ProductResponseItem = {
 	id: string;
@@ -12,6 +13,37 @@ type ProductResponseItem = {
 	};
 	image: string;
 	longDescription: string;
+};
+
+export const executeGraphql = async <TResult, TVariables>(
+	query: TypedDocumentString<TResult, TVariables>,
+	variables: TVariables,
+): Promise<TResult> => {
+	if (!process.env.GRAPHQL_URL) {
+		throw TypeError("GRAPHQL_URL is not defined");
+	}
+
+	const res = await fetch(process.env.GRAPHQL_URL, {
+		method: "POST",
+		body: JSON.stringify({
+			query,
+			variables,
+		}),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	const graphqlResponse =
+		(await res.json()) as GraphQLResponse<TResult>;
+
+	if (graphqlResponse.errors) {
+		throw TypeError(`GraphQL Error`, {
+			cause: graphqlResponse.errors,
+		});
+	}
+
+	return graphqlResponse.data;
 };
 
 export const getProductsList = async (): Promise<
